d28f197324f4bf05d697e76bcd3d82068c3bd27f
docker123 <docker123@gmail.com>
2025-12-13 10:45:57 +0000

    added new project

---
+++ 01 Images and Container/node-project/server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+let userGoal = 'Learn Docker!';
+let userName = '';
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <link rel="stylesheet" href="styles.css">
+      </head>
+      <body>
+        <section>
+          <h2>My Course Goal</h2>
+	  <h3>Hey! ${userName} here is ur set goal</h3>
+          <h3>${userGoal}</h3>
+        </section>
+        <form action="/store-goal" method="POST">
+          <div class="form-control">
+	    <label>Name</label>
+	    <input type="text" name="user">
+            <label>Course Goal</label>
+            <input type="text" name="goal">
+          </div>
+          <button>Set Course Goal</button>
+        </form>
+      </body>
+    </html>
+  `);
+});
+
+app.post('/store-goal', (req, res) => {
+  const uName = req.body.user;
+  const enteredGoal = req.body.goal;
+  console.log(uName,' entered a goal ',enteredGoal);
+  userName = uName;
+  userGoal = enteredGoal;
+  res.redirect('/');
+});
+
+app.listen(80);
